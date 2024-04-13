b4a7e291d5c2c4d7e1aa78d3b80cddec2e4d287f
ppalladio <nine.slide@gmail.com>
2024-04-13 18:58:26 +0200

    pre rendering error

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 import Container from '@/components/ui/Container';
 import useCart from '@/hooks/useCart';
@@ -42,7 +42,10 @@ const CartPage = () => {
                                 ))}
                             </ul>
                         </div>
+						<Suspense>
+
                         <Summary />
+						</Suspense>
                     </div>
                 </div>
             </Container>
